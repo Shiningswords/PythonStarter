3cad1bee7887389b1ee61bd33fcf994a19c2ee0f
GudochkinK <goodok-01@hotmail.com>
2022-11-11 15:04:52 +0300

    Итоговая версия мини-проекта "Шифр Цезаря"

--- Python_Stepik_Tasks/00_Stepik_Beginner/07_Mini_projects/03_caesar_cipher.py
+++ Python_Stepik_Tasks/00_Stepik_Beginner/07_Mini_projects/03_caesar_cipher.py
@@ -32,12 +32,46 @@ def encryption(lang, stp, txt):
     return res
 
 
+def decryption(lang, stp, txt):
+    res = ''
+    if lang == 'Русский':
+        for c in txt:
+            if c in RUS_LOW:
+                if ord(c) - stp < 1072:
+                    res += chr(1104 + ((ord(c) - stp) - 1072))
+                else:
+                    res += chr(ord(c) - stp)
+            if c in RUS_UP:
+                if ord(c) - stp < 1040:
+                    res += chr(1072 + ((ord(c) - stp) - 1040))
+                else:
+                    res += chr(ord(c) - stp)
+            if c not in RUS_LOW and c not in RUS_UP:
+                res += c
+        return res
+    else:
+        for c in txt:
+            if c in ENG_LOW:
+                if ord(c) - stp < 97:
+                    res += chr(123 + ((ord(c) - stp) - 97))
+                else:
+                    res += chr(ord(c) - stp)
+            if c in ENG_UP:
+                if ord(c) - stp < 65:
+                    res += chr(91 + ((ord(c) - stp) - 65))
+                else:
+                    res += chr(ord(c) - stp)
+            if c not in ENG_LOW and c not in ENG_UP:
+                res += c
+    return res
+
+
 RUS_LOW = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
 RUS_UP = 'АБВГДЕЖЗИКЛМНОПРСТУФЦЧШЩЪЫЬЭЮЯ'
 ENG_LOW = 'abcdefghijklmnopqrstuvwxyz'
 ENG_UP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-print('Добро пожаловать в программу "ШИФР ЦЕЗАРЯ')
+print('Добро пожаловать в программу "ШИФР ЦЕЗАРЯ"')
 
 while True:
     cipher = input('Введите "+" для шифрования и "-" для дешифрования: ')
@@ -55,10 +89,17 @@ while True:
 
 while True:
     try:
-        step = int(input('Введите шаг шифрования: '))
-        break
+        step = int(input(f'Введите шаг {"шифрования" if cipher == "+" else "дешифрования"}: '))
+        if step < 0:
+            print('Введите корректные данные')
+        else:
+            break
     except ValueError:
         print('Введите корректные данные')
 
-text = input('Введите текст для шифрования/дешифрования: ')
-print(encryption(language, step, text))
+text = input(f'Введите текст для {"шифрования" if cipher == "+" else "дешифрования"}: ')
+
+if cipher == '+':
+    print(encryption(language, step, text))
+else:
+    print(decryption(language, step, text))
